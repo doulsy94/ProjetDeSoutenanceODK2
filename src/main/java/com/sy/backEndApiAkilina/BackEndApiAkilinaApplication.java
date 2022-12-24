4b6a3ce10e65901edadc3521ce3b027254670575
Doulsy94 <doulsy2021@gmail.com>
2022-12-24 20:37:40 +0000

    Projet initialisé

---
+++ src/main/java/com/sy/backEndApiAkilina/BackEndApiAkilinaApplication.java
@@ -0,0 +1,13 @@
+package com.sy.backEndApiAkilina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApiAkilinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApiAkilinaApplication.class, args);
+	}
+
+}
